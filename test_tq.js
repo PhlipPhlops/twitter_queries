711442e75a301971585ff00b4a8b189f2e3cd76e
PK Morgan <morgana@localhost.localdomain>
2021-01-04 16:29:51 -0500

    Packaged twitter queries into a class

---
+++ test_tq.js
@@ -0,0 +1,4 @@
+let TwitterQueries = require('./twitter_queries.js')
+
+let tq = new TwitterQueries();
+tq.search("atlanta home");
--- twitter_queries.js
+++ twitter_queries.js
@@ -1,45 +1,43 @@
 require('dotenv').config()
-var Twitter = require('twitter');
+let Twitter = require('twitter');
 
-// Requires a .env file containing these variables
-var client = new Twitter({
-    consumer_key: process.env.API_KEY,
-    consumer_secret: process.env.API_KEY_SECRET,
-    access_token_key: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
-});
-
-console.log(process.env.API_KEY)
-
-// This params will be filled in via API call
-var params = {screen_name: 'nodejs'};
-
-// client.get('statuses/user_timeline', params, function(error, tweets, response) {
-//     if (!error) {
-//         console.log(response)
-//     } else {
-//         console.log(error)
-//     }
-// });
-
-params = {
-    q: 'Atlanta',
-    lang: 'en',
-    maxResults: '1'
-}
-// client.get('search/tweets', params, function(error, tweets, response) {
-//     if (!error) {
-//         console.log(tweets)
-//     } else {
-//         console.log(error)
-//     }
-// })
-
-client.get('favorites/list', {}, function(error, tweets, response) {
-    if (!error) {
-        console.log("FAVORITES")
-        console.log(tweets)
-    } else {
-        console.log(error)
+module.exports = class TwitterQueries {
+    constructor() {
+        // Requires a .env file containing these variables
+        this.client = new Twitter({
+            consumer_key: process.env.API_KEY,
+            consumer_secret: process.env.API_KEY_SECRET,
+            
+            // Access only for acocunt keys
+            access_token_key: process.env.ACCESS_TOKEN,
+            access_token_secret: process.env.ACCESS_TOKEN_SECRET
+        });
     }
-})
+
+    _handleError(error) {
+        if (error) console.error(error);
+    }
+
+    getBookmarks() {
+        let bookmarks;
+        this.client.get('favorites/list', {}, (error, tweets, response) => {
+            this._handleError(error);
+            console.log(tweets)
+            bookmarks = tweets;
+        })
+        return bookmarks;
+    }
+
+    search(query, lang='en', maxResults=10) {
+        var params = {
+            q: query,
+            lang: lang,
+            maxResults: maxResults
+        }
+
+        this.client.get('search/tweets', params, (error, tweets, response) => {
+            this._handleError(error);
+            console.log(tweets);
+        })
+    }
+}
